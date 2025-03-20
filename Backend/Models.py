490d4853d7552f9f360780e078a50596ea79e4c6
PetarS09 <162906881+PetarS09@users.noreply.github.com>
2025-03-20 13:43:28 +0200

    Made changes
    Added a registration form a login form and a user model in the database

--- Backend/Models.py
+++ Backend/Models.py
@@ -1,3 +1,17 @@
 from datetime import datetime
 from flask_login import UserMixin
-from __init__ import db,login_manager
+from __init__ import DB,login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,DB.Model):
+    id = DB.Column(DB.Integer,primary_key=True)
+    username = DB.Column(DB.String(20),unique=True,nullable=False)
+    email = DB.Column(DB.String(100),unique=True,nullable=False)
+    password = DB.Column(DB.String(50),nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+    
--- Backend/__init__.py
+++ Backend/__init__.py
@@ -10,4 +10,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-from route import routes
+from Backend import route
