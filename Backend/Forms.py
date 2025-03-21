4efd3ba8229c31bcb3cbb8ff49320721823c300c
Деян <deo.ant.ivanov@gmail.com>
2025-03-21 23:06:34 +0200

    removing Backend

--- Backend/Forms.py
+++ Backend/Forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from Backend.Models import User
+from Models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
